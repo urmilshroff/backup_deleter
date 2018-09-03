2bcf3b43cbf1bc10a99ace7a60b6d7d764cd3208
urmilshroff <urmilshroff@gmail.com>
2018-09-03 20:09:55 +0530

    Working

--- tm_deleter.sh
+++ tm_deleter.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+echo -e "\nAll local backups are:"
+tmutil listlocalsnapshots /
+
+oldest_backup=$(tmutil listlocalsnapshots / | head -1) #stores oldest backup name
+backup_date=${oldest_backup#"com.apple.TimeMachine."} #stores oldest backup date
+
+echo -e "\nOldest backup is from $backup_date. Would you like to delete this backup? Y/n:"
+read choice
+
+case $choice in
+    y)
+    echo "Deleting com.apple.TimeMachine.$backup_date..."
+    sudo tmutil deletelocalsnapshots $backup_date
+    ;;
+    *) echo "No backup(s) deleted"
+esac
